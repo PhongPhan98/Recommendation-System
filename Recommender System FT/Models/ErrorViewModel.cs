b2e480235425d0cb1aa9ede02b72533a075e1839
phongphan <phongpx9x@gmail.com>
2021-01-25 10:56:35 +0700

    commit code

---
+++ Recommender System FT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recommender_System_FT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
