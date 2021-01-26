169612d772e340ff596e97f444aedf541f897991
gia hieu <trangiahieu113@gmail.com>
2021-01-26 09:51:35 +0700

    s

--- Recommender System FT/wwwroot/js/site.js
+++ Recommender System FT/wwwroot/js/site.js
@@ -10,9 +10,22 @@
         $(".popup-topten").show();
     })
     $(".bnt-topten").click(function () {
+        $(".data-topten").hide();
         $(".popup-data").hide();
         $(".popup-topten").show();
+        $(".loadtop10").show();
+        timerId = window.setInterval(function () {
+            x = $("tbody").children();
+            if (x.length >= 1) {
+                $(".loadtop10").hide();
+                $(".data-topten").show();
+            } else { }
+        }, 2000);
+        setTimeout(() => { clearInterval(timerId);}, 30000);
+       
+       
     })
+   
     $(".done").click(function () {
         $(".popup-topten").hide();
         $(".main").show();
